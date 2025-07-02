c276920d6cee2f768a1a297d52e8b7601f6c5dfe
anant <sanant640@gmail.com>
2025-07-02 19:26:24 +0530

    first commit

---
+++ src/main/java/com/learning/JavaappsApplication.java
@@ -0,0 +1,13 @@
+package com.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaappsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaappsApplication.class, args);
+	}
+
+}
